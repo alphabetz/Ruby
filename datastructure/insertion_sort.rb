06523347482f9945dea474986faa5bf72a86b8d0
Alphabetz <a.alphabet@gmail.com>
2015-07-02 20:46:55 +0700

    Move file to correct place

--- datastructure/insertion_sort.rb
+++ datastructure/insertion_sort.rb
@@ -5,13 +5,9 @@ def insertion_sort(list)
   (1..list.length - 1).each do |index|
     value = list[index]
     i = index - 1
-    until i < 0 do 
-      if value < list[i]
-        list[i+1], list[i] = list[i], value
+    while i >= 0 && value < list[i]
+        list[i+1], list[i] = list[i], value #swap
         i = i - 1
-      else
-        break
-      end
     end
   end
 end
